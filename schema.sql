f0ac05221ce61bf1fe19c88af79a81ea6fc83dd5
Grace Durham <gracelee.durham@gmail.com>
2016-12-08 15:18:59 -0800

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists addressbook;
-create table addressbook (
-	id integer primary key autoincrement,
-	login_user text not null,
-	name text not null,
-	phone_num integer not null
-	);
