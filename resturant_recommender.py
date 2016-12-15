28caf2accfa6ac306407dea93c04e982442933c0
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2016-12-14 22:10:44 -0800

    Style changes

--- resturant_recommender.py
+++ resturant_recommender.py
@@ -3,17 +3,21 @@ import yelp_helper
 
 def recommend_based_on_weather(state, city):
 	#get the temp
-	weather = weather_helper.fetch_weather(state, city);
-	
+	temp = weather_helper.fetch_weather(state, city);
+	str_temp = str(temp)
 	#based on the temp we are going to populate a search term for yelp
 	search_term = 'indoor seating'    
-	
-	if(weather > 65):
+	headline = "It feels like " + str_temp + " outside. How about eating somewhere inside. Pick one below."
+
+	if(temp > 65):
 		search_term = 'outdoor seating'
-	elif(weather < 40):
-		search_term = 'soup'
+		headline = "It feels like " + str_temp + " outside. What a great day to go to an outdoor resturant. Pick one below."
+
+	elif(temp < 40):
+		search_term = 'soup'   
+		headline = "It feels like " + str_temp + " outside. Better grub somewhere warm with soup. Pick one below."
 
 	#hit the yelp API for resturants in our area using the search term
 	resturants = yelp_helper.fetch_resturants(state, city, search_term);
 
-	return resturants
+	return {"resturants": resturants, "temp": temp, "headline": headline}
--- resturants.py
+++ resturants.py
@@ -3,15 +3,19 @@ from flask import render_template
 from flask import request, url_for, redirect, session, g, flash
 import resturant_recommender
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/recomendations', methods=['GET'])
 def list_recomendations():
 	state = request.args.get("state")
 	city = request.args.get("city")
 
-	resturants = resturant_recommender.recommend_based_on_weather(state, city)
-	return render_template('recomendations/list.html', resturants=resturants, state=state, city=city)
+	recommendation_info = resturant_recommender.recommend_based_on_weather(state, city)
+	resturants = recommendation_info["resturants"]
+	temp = recommendation_info["temp"]
+	headline = recommendation_info["headline"]
+
+	return render_template('recomendations/list.html', headline=headline, temp=temp, resturants=resturants, state=state, city=city)
 
 
 @app.route('/', methods=['GET'])
