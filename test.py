0eef3ac266202660bb4027aea21a5ab42751897d
Grace Durham <gracelee.durham@gmail.com>
2017-12-01 12:02:00 -0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import resturant_recommender
-
-resturants = resturant_recommender.recommend_based_on_weather("MO", "Kansas City")
-
-for resturant in resturants:
-	print resturant.name
-	print resturant.image_url
-	print 
-
