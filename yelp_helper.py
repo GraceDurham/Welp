2fd10aecffa3916d1caa45860d9ee38d0aa3872e
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2017-12-01 16:17:07 -0800

    update token type

--- yelp_helper.py
+++ yelp_helper.py
@@ -7,7 +7,7 @@ from yelp.oauth1_authenticator import Oauth1Authenticator
 auth = Oauth1Authenticator(
     consumer_key=YOUR_CONSUMER_KEY,
     consumer_secret=YOUR_CONSUMER_SECRET,
-    # token=YOUR_TOKEN,
+    token=YOUR_TOKEN,
     token_secret=YOUR_TOKEN_SECRET
 )
 
